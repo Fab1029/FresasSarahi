0f3c31a161604751a723410af80e6122c13a2f44
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-08-28 14:45:35 -0500

    (fix): Footer Icons Fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,16 +10,27 @@ const Footer = () => {
 
   return (
     <div className='footer'>
+        {/* Izquierda */}
         <div className='left-footer-container'>
           <img src={logo} alt='logo' className='logo-footer' loading='lazy'/>
           <div className='social-media-icons-container'>
-            <img src={facebookIcon} alt='facebook-icon' loading='lazzy' className='social-icon facebook-icon'/>
-            <img src={instagramIcon} alt='instagram-icon' loading='lazzy' className='social-icon'/>
-            <img src={whatsappIcon} alt='whatsapp-icon' loading='lazzy' className='social-icon'/>
+            
+            <button>
+              <img src={facebookIcon} alt='facebook-icon' loading='lazy' className='social-icon facebook-icon'/>
+            </button>
+
+            <button>
+              <img src={instagramIcon} alt='instagram-icon' loading='lazy' className='social-icon'/>
+            </button>
+
+            <button>
+              <img src={whatsappIcon} alt='whatsapp-icon' loading='lazy' className='social-icon'/>
+            </button>
+            
           </div>
         </div>      
         
-
+        {/* Centro */}
         <div className='center-footer-container'>
           <div className='footer-info-container'>
             <h1>Categorías</h1>
@@ -34,12 +45,17 @@ const Footer = () => {
               <p>+593 0998956672</p>
               <p>Nabón, Azuay, Ecuador</p>
           </div>
-
         </div>
 
-        <div className='right-footer-container'/>
-        
-
+        {/* Derecha (Mapa Google) */}
+        <div className='right-footer-container'>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.901203124!2d-79.0!3d-2.9333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91cd1234567890%3A0xabcdef!2sNab%C3%B3n%2C%20Azuay%2C%20Ecuador!5e0!3m2!1ses!2sec!4v1700000000000!5m2!1ses!2sec"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
     </div>
   )
 }
