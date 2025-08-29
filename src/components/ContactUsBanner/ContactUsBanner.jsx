7fe4bf5ba94422be6c951503ca883fef85896879
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-08-29 10:28:18 -0500

    (fix): Contact Us Banner and Home Banner changed img to svg

--- src/components/ContactUsBanner/ContactUsBanner.jsx
+++ src/components/ContactUsBanner/ContactUsBanner.jsx
@@ -13,7 +13,13 @@ const ContactUsBanner = () => {
             <h1>{'¿Antojado de fresas\nfrescas y orgánicas\ntodos los días?'}</h1>
             <button>
                 Contactate con nosotros
-                <img src={rightArrowIcon}/>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 24 24" 
+                    fill="currentColor" 
+                    role="img" aria-label="Flecha derecha">
+                  <path d="M12.3 4.3a1 1 0 0 1 1.4 0l7 7a1 1 0 0 1 0 1.4l-7 7a1 1 0 0 1-1.4-1.4L17.6 13H4a1 1 0 0 1 0-2h13.6l-5.3-5.3a1 1 0 0 1 0-1.4Z"/>
+                </svg>
+
             </button>
         </div>
         <img src={logo} alt='logo' loading='lazy' className='contact-us-right-container'/>
